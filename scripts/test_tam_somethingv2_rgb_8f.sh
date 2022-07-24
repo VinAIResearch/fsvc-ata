a8a62973a36ef0438bce5e136bc13609a8ce689c
Khoi D. Nguyen <47294247+khoiucd@users.noreply.github.com>
2022-07-24 22:12:04 +0800

    Update test_tam_somethingv2_rgb_8f.sh

--- scripts/test_tam_somethingv2_rgb_8f.sh
+++ scripts/test_tam_somethingv2_rgb_8f.sh
@@ -1,5 +1,4 @@
 # test on Something-Something V2
-export CUDA_VISIBLE_DEVICES=2
 python -u main.py somethingv2 RGB --arch resnet50 --num_segments 8 -j 2 \
 --root_log ./checkpoints/these_ckpt --root_model ./checkpoints/these_ckpt --npb \
 --resume ./../somethingv2.path.tar --evaluate --gpus 0 --iter 50 --shot 1
