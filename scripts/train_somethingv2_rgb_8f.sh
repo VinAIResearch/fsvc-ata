5ea78e57ec38b066c5be449274b795e6b973cfc9
Khoi D. Nguyen <47294247+khoiucd@users.noreply.github.com>
2022-07-26 18:08:26 +0800

    Update train_somethingv2_rgb_8f.sh

--- scripts/train_somethingv2_rgb_8f.sh
+++ scripts/train_somethingv2_rgb_8f.sh
@@ -1,5 +1,6 @@
+# train on Something-Something V2
 python -u main.py somethingv2 RGB --arch resnet50 \
---num_segments 8 --lr 0.001 --lr_steps 10 20 --epochs 25 --batch-size 32 \
--j 2 --dropout 0.5 --root_log ./checkpoints/path \
---root_model ./checkpoints/path --eval-freq=1 --npb \
---wd 0.0005 --print-freq 200 --gpus 0 --episodes 600
+--num_segments 8 --lr 0.001 --lr_steps 10 20 --epochs 25  \
+--batch-size 32 --workers 2 --dropout 0.5 \
+--root_log ./checkpoints/path --root_model ./checkpoints/path \
+--wd 0.0005 --gpus 0 --episodes 600
