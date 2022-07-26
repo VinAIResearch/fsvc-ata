ad17aaa1500f07a28f1e64e797b10137d6af8cd1
Khoi D. Nguyen <47294247+khoiucd@users.noreply.github.com>
2022-07-26 18:07:36 +0800

    Update test_somethingv2_rgb_8f.sh

--- scripts/test_somethingv2_rgb_8f.sh
+++ scripts/test_somethingv2_rgb_8f.sh
@@ -1,4 +1,4 @@
 # test on Something-Something V2
-python -u main.py somethingv2 RGB --arch resnet50 --num_segments 8 -j 2 \
---root_log ./checkpoints/path --root_model ./checkpoints/path --npb \
---resume ./../somethingv2.path.tar --evaluate --gpus 0 --iter 50 --shot 1
+python -u main.py somethingv2 RGB --arch resnet50 --num_segments 8 --workers 2 \
+--root_log ./checkpoints/path --root_model ./checkpoints/path \
+--resume ./../somethingv2.path.tar --evaluate --gpus 0 --way 5 --shot 1 --episodes 10000
